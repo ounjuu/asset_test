e66577c6111e63f34fdea9d3178aeaf74308efd4
ounjuu <koj1555@naver.com>
2025-08-09 17:06:00 +0900

    Feat: Setting 1

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl font-bold">
+      TailwindCSS v3 OK!
+    </div>
+  );
+}
