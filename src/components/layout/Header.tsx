079ec69b267535d8038b136a270cbbdb6c604c25
ounjuu <koj1555@naver.com>
2025-08-12 12:37:00 +0900

    Fix: header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,10 @@ export default function Header() {
       <div className="px-[10px] mx-auto flex justify-between items-center h-[42px] w-full">
         {/* 로고 */}
         <div className="flex">
-          <div className="flex items-center gap-2 cursor-pointer">
+          <div
+            className="flex items-center gap-2 cursor-pointer"
+            onClick={() => (window.location.href = "/")}
+          >
             <img
               src="/quantusLogo.ced3539c.svg"
               alt="Logo"
@@ -17,13 +20,22 @@ export default function Header() {
             <div className="flex justify-center items-center pt-[5px] cursor-pointer hover:text-gray-500">
               <span>파운드리</span>
             </div>
-            <div className="flex justify-center items-center pt-[5px] cursor-pointer hover:text-gray-500">
+            <div
+              className="flex justify-center items-center pt-[5px] cursor-pointer hover:text-gray-500"
+              onClick={() => alert("준비중입니다")}
+            >
               고객지원
             </div>
-            <div className="flex justify-center items-center pt-[5px] cursor-pointer hover:text-gray-500">
+            <div
+              className="flex justify-center items-center pt-[5px] cursor-pointer hover:text-gray-500"
+              onClick={() => alert("준비중입니다")}
+            >
               사용권 구매
             </div>
-            <div className="flex justify-center items-center pt-[5px] w-[54px] h-[34px] cursor-pointer hover:opacity-80">
+            <div
+              className="flex justify-center items-center pt-[5px] w-[54px] h-[34px] cursor-pointer hover:opacity-80"
+              onClick={() => alert("준비중입니다")}
+            >
               <img
                 src="/headerVIP.webp"
                 alt="Logo"
@@ -35,13 +47,18 @@ export default function Header() {
 
         {/* 버튼 */}
         <div className="hidden md:block">
-          <button className="px-4 py-2 rounded-md transition bg-white text-black text-sm font-bold">
+          <button
+            onClick={() => alert("준비중입니다")}
+            className="px-4 py-2 rounded-md transition bg-white text-black text-sm font-bold"
+          >
             로그인
           </button>
         </div>
 
         {/* 모바일 메뉴 버튼 */}
-        <button className="md:hidden ">☰</button>
+        <button className="md:hidden" onClick={() => alert("준비중입니다")}>
+          ☰
+        </button>
       </div>
     </header>
   );
